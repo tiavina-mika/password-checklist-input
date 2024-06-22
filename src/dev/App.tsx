86cca2b5cbc351c1948af37ab2c95c11001e53b7
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 18:15:50 +0300

    Update App.tsx

--- src/dev/App.tsx
+++ src/dev/App.tsx
@@ -1,28 +1,23 @@
-import { Container, ThemeProvider, createTheme } from '@mui/material';
 import PasswordChecklist from '../PasswordChecklist';
-
-const theme = createTheme();
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <PasswordChecklist
-          className='input'
-          validationMessages={{
-            minLength: 'Devrait contenir au moins 8 caractères',
-            lowerCase: 'Devrait contenir au moins une lettre minuscule',
-            upperCase: 'Devrait contenir au moins une lettre majuscule',
-            number: 'Devrait contenir au moins un chiffre',
-            specialCharacters: 'Devrait contenir au moins un caractère spécial',
-          }}
-          options={{
-            minLength: 6,
-            allowedSpecialChar: "="
-          }}
-        />
-      </Container>
-    </ThemeProvider>
+    <div>
+      <PasswordChecklist
+        className='input'
+        validationMessages={{
+          minLength: 'Devrait contenir au moins 8 caractères',
+          lowerCase: 'Devrait contenir au moins une lettre minuscule',
+          upperCase: 'Devrait contenir au moins une lettre majuscule',
+          number: 'Devrait contenir au moins un chiffre',
+          specialCharacters: 'Devrait contenir au moins un caractère spécial',
+        }}
+        options={{
+          minLength: 6,
+          allowedSpecialChar: "="
+        }}
+      />
+    </div>
   )
 }
 
