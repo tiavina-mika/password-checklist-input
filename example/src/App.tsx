2829c543147db1eba4fe5c8499644cd13cac8c7e
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 20:07:18 +0300

    feat: fix build

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,5 @@
-import { Card, CardContent, Container, ThemeProvider, Typography, createTheme } from '@mui/material';
 import PasswordChecklist from 'password-checklist-input';
 import { ChangeEvent, useState } from 'react';
-
-const theme = createTheme();
 
 const App = () => {
   const [password, setPassword] = useState<string>('');
@@ -12,39 +9,35 @@ const App = () => {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
-        <Card sx={{ maxWidth: 400, pb: 2 }}>
-          <CardContent >
-            <div className="flex flex-col gap-2 mb-16 mt-2">
-              <Typography variant="h5">password-checklist-input</Typography>
-              <Typography>Click on the input field and type a password to see the chack list</Typography>
-            </div>
-            <PasswordChecklist
-              value={password}
-              onChange={handlePasswordChange}
-              // override class name
-              className='input'
-              // override error messages
-              validationMessages={{
-                minLength: 'Devrait contenir au moins 6 caractères',
-                lowerCase: 'Devrait contenir au moins une lettre minuscule',
-                upperCase: 'Devrait contenir au moins une lettre majuscule',
-                number: 'Devrait contenir au moins un chiffre',
-                specialCharacters: 'Devrait contenir au moins un caractère spécial',
-              }}
-              // override options
-              options={{
-                minLength: 6,
-                allowedSpecialChar: "="
-              }}
-              // override TextFieldProps
-              fullWidth
-            />
-          </CardContent>
-        </Card>
-      </Container>
-    </ThemeProvider>
+    <div className="flex flex-col items-center mt-2">
+      <div className="max-w-[400px] pb-2">
+        <div className="flex flex-col gap-2 mb-16 mt-2">
+          <h5 className="text-md font-bold">password-checklist-input</h5>
+          <span>Click on the input field and type a password to see the chack list</span>
+        </div>
+        <PasswordChecklist
+          value={password}
+          onChange={handlePasswordChange}
+          // override class name
+          className='input'
+          // override error messages
+          validationMessages={{
+            minLength: 'Devrait contenir au moins 6 caractères',
+            lowerCase: 'Devrait contenir au moins une lettre minuscule',
+            upperCase: 'Devrait contenir au moins une lettre majuscule',
+            number: 'Devrait contenir au moins un chiffre',
+            specialCharacters: 'Devrait contenir au moins un caractère spécial',
+          }}
+          // override options
+          options={{
+            minLength: 6,
+            allowedSpecialChar: "="
+          }}
+          // override TextFieldProps
+          placeholder="Enter your password"
+        />
+      </div>
+    </div>
   )
 }
 
