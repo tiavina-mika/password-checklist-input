2ec0c7d4ca526bbcb9b466f168bc1897ce6cc2b1
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 18:16:05 +0300

    Update PasswordChecklist.tsx

--- src/PasswordChecklist.tsx
+++ src/PasswordChecklist.tsx
@@ -1,15 +1,13 @@
-import { ChangeEvent, forwardRef, useState } from 'react';
-
-import { IconButton, TextFieldProps, TextField, Theme, useTheme, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { ChangeEvent, InputHTMLAttributes, forwardRef, useState } from 'react';
 
 import VisibilityOff from './icons/VisibilityOff';
 import Visibility from './icons/Visibility';
 import Check from './icons/Check';
 import Close from './icons/Close';
-import { PasswordChecklistProps } from './types';
 import { validatePasswordChecklist, PasswordCheckListResult } from 'validate-password-checklist';
+import { PasswordChecklistProps } from './types';
 
-const PasswordChecklist =  forwardRef<HTMLDivElement, PasswordChecklistProps & TextFieldProps>(({
+const PasswordChecklist =  forwardRef<HTMLInputElement, PasswordChecklistProps & InputHTMLAttributes<HTMLInputElement>>(({
   options,
   className,
   hidePasswordIcon,
@@ -19,8 +17,6 @@ const PasswordChecklist =  forwardRef<HTMLDivElement, PasswordChecklistProps & T
 }, ref) => {
   const [rules, setRules] = useState<PasswordCheckListResult['validationMessages']>([]);
   const [showPassword, setShowPassword] = useState<boolean>(false);
-
-  const theme = useTheme();
 
   const toggleShowPassword = () => setShowPassword(!showPassword);
 
@@ -39,56 +35,53 @@ const PasswordChecklist =  forwardRef<HTMLDivElement, PasswordChecklistProps & T
       {/* ------------------------------------------- */}
       {/* ---------------- text field --------------- */}
       {/* ------------------------------------------- */}
-      <TextField
-        ref={ref}
-        {...rest}
-        className={className}
-        type={showPassword ? 'text' : 'password'}
-        onChange={handleChange}
-        InputProps={{
-          endAdornment: (
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={toggleShowPassword}
-              edge="end"
-            >
-              {/* toggle eye icon */}
-              {showPassword
-                ? (hidePasswordIcon || <VisibilityOff />)
-                : (showPasswordIcon || <Visibility />)
-              }
-            </IconButton>
-          ),
-        }}
-      />
+      <div className="input-container">
+        <input
+          ref={ref}
+          {...rest}
+          className={className}
+          type={showPassword ? 'text' : 'password'}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={toggleShowPassword} className="visibility-icon-button">
+          {showPassword
+            ? (hidePasswordIcon || <VisibilityOff />)
+            : (showPasswordIcon || <Visibility />)
+          }
+        </button>
+      </div>
 
       {/* ------------------------------------------- */}
       {/* ------ password requirement checklist ----- */}
       {/* ------------------------------------------- */}
       {rules.length > 0 && (
-        <List sx={{ p: 0, mt: 1 }}>
+        <ul>
           {rules.map((error, index) => (
-              <ListItem key={index} sx={{ padding: 0 }}>
-                <ListItemIcon sx={{ minWidth: 24, '& svg': { width: 18 } }}>
+              <li key={index}>
+              {/* <li key={index} sx={{ padding: 0 }}> */}
+                <span>
+                {/* <span sx={{ minWidth: 24, '& svg': { width: 18 } }}> */}
                   {/* ------ left icon ------ */}
                   {error.passed
-                    ? <Check fill={theme.palette.success.main} />
-                    : <Close fill={theme.palette.error.main} />
+                    // ? <Check fill={theme.palette.success.main} />
+                    // : <Close fill={theme.palette.error.main} />
+                    ? <Check fill="red" />
+                    : <Close fill="green" />
                   }
-                </ListItemIcon>
+                </span>
                 {/* ------ error message ------ */}
-                <ListItemText
-                  sx={{ color: (theme: Theme) => error.passed
-                    ? theme.palette.success.main
-                    : theme.palette.error.main
-                  }}
+                <span
+                  // sx={{ color: (theme: Theme) => error.passed
+                  //   ? theme.palette.success.main
+                  //   : theme.palette.error.main
+                  // }}
                 >
                   {error.message}
-                </ListItemText>
-              </ListItem>
+                </span>
+              </li>
             )
           )}
-        </List>
+        </ul>
       )}
     </>
   );
