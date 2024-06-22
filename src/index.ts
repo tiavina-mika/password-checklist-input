544f6f95527ed668a080be0b30c28a0165481851
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 17:51:10 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import PasswordChecklist from './PasswordChecklist';
+
+export {
+  type PasswordChecklistProps as PasswordStrengthInputProps
+} from './types.d';
+
+export default PasswordChecklist;
