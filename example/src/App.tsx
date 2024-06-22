f5e73956343732c7aeeecc855f7aabd74e34f1ff
tiavina-mika <tiavinamika@gmail.com>
2024-06-22 18:54:31 +0300

    Create App.tsx

---
+++ example/src/App.tsx
@@ -0,0 +1,51 @@
+import { Card, CardContent, Container, ThemeProvider, Typography, createTheme } from '@mui/material';
+import PasswordChecklist from 'password-checklist-input';
+import { ChangeEvent, useState } from 'react';
+
+const theme = createTheme();
+
+const App = () => {
+  const [password, setPassword] = useState<string>('');
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
+        <Card sx={{ maxWidth: 400, pb: 2 }}>
+          <CardContent >
+            <div className="flex flex-col gap-2 mb-16 mt-2">
+              <Typography variant="h5">password-checklist-input</Typography>
+              <Typography>Click on the input field and type a password to see the chack list</Typography>
+            </div>
+            <PasswordChecklist
+              value={password}
+              onChange={handlePasswordChange}
+              // override class name
+              className='input'
+              // override error messages
+              validationMessages={{
+                minLength: 'Devrait contenir au moins 6 caractères',
+                lowerCase: 'Devrait contenir au moins une lettre minuscule',
+                upperCase: 'Devrait contenir au moins une lettre majuscule',
+                number: 'Devrait contenir au moins un chiffre',
+                specialCharacters: 'Devrait contenir au moins un caractère spécial',
+              }}
+              // override options
+              options={{
+                minLength: 6,
+                allowedSpecialChar: "="
+              }}
+              // override TextFieldProps
+              fullWidth
+            />
+          </CardContent>
+        </Card>
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
